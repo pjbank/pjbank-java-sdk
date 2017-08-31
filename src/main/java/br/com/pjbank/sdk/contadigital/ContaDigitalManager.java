c4f8092fbe39519e239b4db62b23a37446e8021f
Vinícius Silva <vinicius768@msn.com>
2017-08-31 16:55:01 -0300

    Início da impĺementação de transferência entre conta e subconta

--- src/main/java/br/com/pjbank/sdk/contadigital/ContaDigitalManager.java
+++ src/main/java/br/com/pjbank/sdk/contadigital/ContaDigitalManager.java
@@ -315,4 +315,52 @@ public class ContaDigitalManager extends PJBankAuthenticatedService {
 
         return transacoesExtrato;
     }
+
+    /**
+     * Realiza uma transferência da Conta Digital para um Subconta Digital
+     * @param transacoesTransferenciasContaSubconta: Transferências à serem realizadas
+     * @return List<ResponseTransferencia>: Lista com retorno de cada pagamento do lote
+     */
+    // TODO: Refactoring em model de transferência e seu response para se adaptar melhor a este código
+    public List<ResponsePagamento> accountToSubaccountTransfer(List<TransacaoCodigoBarras> transacoesTransferenciasContaSubconta) throws IOException, ParseException, PJBankException {
+        PJBankClient client = new PJBankClient(this.endPoint.replace("{{credencial-conta}}", this.credencial).concat("/transacoes"));
+        HttpPost httpPost = client.getHttpPostClient();
+        httpPost.addHeader("x-chave-conta", this.chave);
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
+        JSONArray despesasArray = new JSONArray();
+
+        for (TransacaoCodigoBarras transacaoCodigoBarras : transacoesTransferenciasContaSubconta) {
+            JSONObject despesaObject = new JSONObject();
+            despesaObject.put("data_vencimento", dateFormat.format(transacaoCodigoBarras.getDataVencimento()));
+            despesaObject.put("valor", transacaoCodigoBarras.getValor());
+            despesaObject.put("conta_destino", transacaoCodigoBarras.getCodigoBarras());
+
+            despesasArray.put(despesaObject);
+        }
+
+        JSONObject params = new JSONObject();
+        params.put("lote", despesasArray);
+
+        httpPost.setEntity(new StringEntity(params.toString(), StandardCharsets.UTF_8));
+
+        String response = EntityUtils.toString(client.doRequest(httpPost).getEntity());
+
+        JSONArray responseArray = new JSONObject(response).getJSONArray("data");
+        List<ResponsePagamento> responsesPagamentos = new ArrayList<>();
+
+        for(int i = 0; i < responseArray.length(); i++) {
+            JSONObject object = (JSONObject) responseArray.get(i);
+
+            ResponsePagamento responsePagamento = new ResponsePagamento();
+            responsePagamento.setIdOperacao(object.getString("id_operacao"));
+            responsePagamento.setStatus(object.getInt("status"));
+            responsePagamento.setMessage(object.getString("msg"));
+
+            responsesPagamentos.add(responsePagamento);
+        }
+
+        return responsesPagamentos;
+    }
+
 }
