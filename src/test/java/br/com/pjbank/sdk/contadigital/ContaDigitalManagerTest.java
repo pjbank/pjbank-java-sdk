5d31c0f1248c5242a8eff244e057f15205ed1b7c
Vinícius Silva <vinicius768@msn.com>
2017-09-04 11:03:22 -0300

    Implementação de teste de inserção de carga em conta digital

--- src/test/java/br/com/pjbank/sdk/contadigital/ContaDigitalManagerTest.java
+++ src/test/java/br/com/pjbank/sdk/contadigital/ContaDigitalManagerTest.java
@@ -1,17 +1,19 @@
 package br.com.pjbank.sdk.contadigital;
 
-import br.com.pjbank.sdk.enums.FormatoExtrato;
-import br.com.pjbank.sdk.enums.TipoConta;
 import br.com.pjbank.sdk.exceptions.PJBankException;
-import br.com.pjbank.sdk.models.contadigital.TransacaoTransferencia;
-import br.com.pjbank.sdk.models.contadigital.TransacaoTransferenciaInterna;
+import br.com.pjbank.sdk.models.common.Boleto;
 import org.json.JSONException;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.text.ParseException;
-import java.util.*;
+
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * @author Vinícius Silva <vinicius.silva@superlogica.com>
@@ -19,8 +21,35 @@ import java.util.*;
  * @since 1.0
  */
 public class ContaDigitalManagerTest {
+    private String credencial;
+    private String chave;
+
+    @Before
+    public void init() {
+        this.credencial = "eb2af021c5e2448c343965a7a80d7d090eb64164";
+        this.chave = "a834d47e283dd12f50a1b3a771603ae9dfd5a32c";
+    }
+
     @Test
-    public void addBalance() throws IOException, JSONException, PJBankException {
+    public void addBalanceComValorValido() throws IOException, JSONException, PJBankException {
+        ContaDigitalManager contaDigitalManager = new ContaDigitalManager(this.credencial, this.chave);
+
+        Boleto boleto = contaDigitalManager.addBalance(25.50);
+
+        Assert.assertThat(boleto.getNossoNumero(), not(is(emptyOrNullString())));
+        Assert.assertThat(boleto.getLinkBoleto(), not(is(emptyOrNullString())));
+        Assert.assertThat(boleto.getLinhaDigitavel(), not(is(emptyOrNullString())));
+    }
+
+    @Test(expected = PJBankException.class)
+    public void addBalanceComValorMenorQueVinteECincoReais() throws IOException, JSONException, PJBankException {
+        ContaDigitalManager contaDigitalManager = new ContaDigitalManager(this.credencial, this.chave);
+
+        Boleto boleto = contaDigitalManager.addBalance(1.50);
+
+        Assert.assertThat(boleto.getNossoNumero(), not(is(emptyOrNullString())));
+        Assert.assertThat(boleto.getLinkBoleto(), not(is(emptyOrNullString())));
+        Assert.assertThat(boleto.getLinhaDigitavel(), not(is(emptyOrNullString())));
     }
 
     @Test
