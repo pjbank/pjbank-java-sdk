2ae79a687550646a57f4cc75bb7f4dd4d9937993
Vinicius Silva <vinicius768@msn.com>
2017-09-26 14:05:13 -0300

    Adição de arquivo para manter as configurações de testes, como credenciamento e outras

---
+++ src/test/java/br/com/pjbank/sdk/api/PJBankConfigTest.java
@@ -0,0 +1,38 @@
+package br.com.pjbank.sdk.api;
+
+/**
+ * @author Vinícius Silva <vinicius.silva@superlogica.com>
+ * @version 1.0
+ * @since 1.0
+ */
+public class PJBankConfigTest {
+    /**
+     * Credencial para boletos (Conta de Recebimento)
+     */
+    public final static String credencialBoletosContaRecebimento = "d3418668b85cea70aa28965eafaf927cd34d004c";
+
+    /**
+     * Chave para boletos (Conta de Recebimento)
+     */
+    public final static String chaveBoletosContaRecebimento = "46e79d6d5161336afa7b98f01236efacf5d0f24b";
+
+    /**
+     * Credencial para cartão de crédito (Conta de Recebimento)
+     */
+    public final static String credencialCartaoCreditoContaRecebimento = "1264e7bea04bb1c24b07ace759f64a1bd65c8560";
+
+    /**
+     * Chave para cartão de crédito (Conta de Recebimento)
+     */
+    public final static String chaveCartaoCreditoContaRecebimento = "ef947cf5867488f744b82744dd3a8fc4852e529f";
+
+    /**
+     * Credencial para conta digital
+     */
+    public final static String credencialContaDigital = "eb2af021c5e2448c343965a7a80d7d090eb64164";
+
+    /**
+     * Chave para conta digital
+     */
+    public final static String chaveContaDigital = "a834d47e283dd12f50a1b3a771603ae9dfd5a32c";
+}
