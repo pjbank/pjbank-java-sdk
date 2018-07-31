f131eeffa5e077c09292a7d3767e7473752b08ab
Vinicius Silva <vinicius768@msn.com>
2018-07-31 14:05:16 -0300

    Added value fields to extract

--- src/main/java/br/com/pjbank/sdk/models/recebimento/ExtratoBoleto.java
+++ src/main/java/br/com/pjbank/sdk/models/recebimento/ExtratoBoleto.java
@@ -3,107 +3,124 @@ package br.com.pjbank.sdk.models.recebimento;
 import java.util.Date;
 
 /**
- * 
  * @author Paulo Vitor Braga Pessoa <foxpv213@gmail.com>
- *
  */
 public class ExtratoBoleto {
-	private double valor;
-	private String nossoNumero;
-	private String nossoNumeroOriginal;
-	private String bancoNumero;
-	private String tokenFacilitador;
-	private double valorLiquido;
-	private Date dataVencimento;
-	private Date dataPagamento;
-	private Date dataCredito;
-	
-	public ExtratoBoleto() {
-	}
+    private double valor;
+    private double valorPago;
+    private double valorLiquido;
+    private double valorTarifa;
+    private String nossoNumero;
+    private String nossoNumeroOriginal;
+    private String bancoNumero;
+    private String tokenFacilitador;
+    private Date dataVencimento;
+    private Date dataPagamento;
+    private Date dataCredito;
 
-	public ExtratoBoleto(double valor, String nossoNumero, String nossoNumeroOriginal, String bancoNumero,
-			String tokenFacilitador, double valorLiquido) {
-		super();
-		this.valor = valor;
-		this.nossoNumero = nossoNumero;
-		this.nossoNumeroOriginal = nossoNumeroOriginal;
-		this.bancoNumero = bancoNumero;
-		this.tokenFacilitador = tokenFacilitador;
-		this.valorLiquido = valorLiquido;
-	}
+    public ExtratoBoleto() {
+    }
 
-	public double getValor() {
-		return valor;
-	}
+    public ExtratoBoleto(double valor, double valorPago, double valorLiquido, double valorTarifa, String nossoNumero,
+                         String nossoNumeroOriginal, String bancoNumero, String tokenFacilitador) {
+        this.valor = valor;
+        this.valorPago = valorPago;
+        this.valorLiquido = valorLiquido;
+        this.valorTarifa = valorTarifa;
+        this.nossoNumero = nossoNumero;
+        this.nossoNumeroOriginal = nossoNumeroOriginal;
+        this.bancoNumero = bancoNumero;
+        this.tokenFacilitador = tokenFacilitador;
+        this.valorLiquido = valorLiquido;
+    }
 
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
+    public double getValor() {
+        return valor;
+    }
 
-	public String getNossoNumero() {
-		return nossoNumero;
-	}
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 
-	public void setNossoNumero(String nossoNumero) {
-		this.nossoNumero = nossoNumero;
-	}
+    public double getValorPago() {
+        return valorPago;
+    }
 
-	public String getNossoNumeroOriginal() {
-		return nossoNumeroOriginal;
-	}
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
 
-	public void setNossoNumeroOriginal(String nossoNumeroOriginal) {
-		this.nossoNumeroOriginal = nossoNumeroOriginal;
-	}
+    public double getValorTarifa() {
+        return valorTarifa;
+    }
 
-	public String getBancoNumero() {
-		return bancoNumero;
-	}
+    public void setValorTarifa(double valorTarifa) {
+        this.valorTarifa = valorTarifa;
+    }
 
-	public void setBancoNumero(String bancoNumero) {
-		this.bancoNumero = bancoNumero;
-	}
+    public double getValorLiquido() {
+        return valorLiquido;
+    }
 
-	public String getTokenFacilitador() {
-		return tokenFacilitador;
-	}
+    public void setValorLiquido(double valorLiquido) {
+        this.valorLiquido = valorLiquido;
+    }
 
-	public void setTokenFacilitador(String tokenFacilitador) {
-		this.tokenFacilitador = tokenFacilitador;
-	}
+    public String getNossoNumero() {
+        return nossoNumero;
+    }
 
-	public double getValorLiquido() {
-		return valorLiquido;
-	}
+    public void setNossoNumero(String nossoNumero) {
+        this.nossoNumero = nossoNumero;
+    }
 
-	public void setValorLiquido(double valorLiquido) {
-		this.valorLiquido = valorLiquido;
-	}
+    public String getNossoNumeroOriginal() {
+        return nossoNumeroOriginal;
+    }
 
-	public Date getDataVencimento() {
-		return dataVencimento;
-	}
+    public void setNossoNumeroOriginal(String nossoNumeroOriginal) {
+        this.nossoNumeroOriginal = nossoNumeroOriginal;
+    }
 
-	public void setDataVencimento(Date dataVencimento) {
-		this.dataVencimento = dataVencimento;
-	}
+    public String getBancoNumero() {
+        return bancoNumero;
+    }
 
-	public Date getDataPagamento() {
-		return dataPagamento;
-	}
+    public void setBancoNumero(String bancoNumero) {
+        this.bancoNumero = bancoNumero;
+    }
 
-	public void setDataPagamento(Date dataPagamento) {
-		this.dataPagamento = dataPagamento;
-	}
+    public String getTokenFacilitador() {
+        return tokenFacilitador;
+    }
 
-	public Date getDataCredito() {
-		return dataCredito;
-	}
+    public void setTokenFacilitador(String tokenFacilitador) {
+        this.tokenFacilitador = tokenFacilitador;
+    }
 
-	public void setDataCredito(Date dataCredito) {
-		this.dataCredito = dataCredito;
-	}
-	
-	
-	
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public Date getDataCredito() {
+        return dataCredito;
+    }
+
+    public void setDataCredito(Date dataCredito) {
+        this.dataCredito = dataCredito;
+    }
+
+
 }
